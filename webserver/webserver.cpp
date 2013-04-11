4cbd372afa6e700da53b0369c00bb27a50ae2f91
davidsteinsland <david@davidsteinsland.net>
2013-04-12 00:03:06 +0200

    Added use of response class

--- webserver/webserver.cpp
+++ webserver/webserver.cpp
@@ -41,11 +41,11 @@ int webserver::webserver::listen ()
 {
 	if (listenSocket->listen(port) != 1)
 	{
-		std::cout << "Error creating socket" << std::endl;
+		// std::cout << "Error creating socket" << std::endl;
 		return 0;
 	}
 	
-	std::cout << "WebServer listening..." << std::endl;
+	// std::cout << "WebServer listening..." << std::endl;
 	
 	while (true)
 	{
@@ -57,7 +57,6 @@ int webserver::webserver::listen ()
 		http::request* request = client->get_request();
 		
 		std::cout << "Got a connection!" << std::endl;
-		// std::cout << request->body() << std::endl;
 		std::cout << "URL: " << request->get_url() << std::endl;
 		std::cout << "URI: " << request->get_uri() << std::endl;
 		std::cout << "QS: " << request->get_query_string() << std::endl;
@@ -67,21 +66,21 @@ int webserver::webserver::listen ()
 		std::string filename = html_dir + request->get_uri();
 		// std::string file = utils::ioutils::get_file_contents ("public_html/index.html");
 		
-		std::string contents;
-		
 		/**
-		 * Try to load the following files, in priority:
+		 * Try to load the following files, in prioritized order:
 		 * 1. The requested file
 		 * 2. Directory index
 		 * 3. 404 page
 		 */
 		std::string filenames[3] = {filename, filename + "index.html", html_dir + "/404.html"};
+		std::string contents;
 		bool file_found = false;
 		for (int i = 0; i < 3; i++)
 		{
 			try
 			{
-				contents = utils::ioutils::get_file_contents(filenames[i]);
+				filename = filenames[i];
+				contents = utils::ioutils::get_file_contents(filename);
 				file_found = true;
 				break;
 			}
@@ -89,29 +88,36 @@ int webserver::webserver::listen ()
 			{ }
 		}
 		
-		std::string headers;		
-	
+		std::string content_type = utils::ioutils::get_mime_type (filename.substr(filename.find_last_of(".")));	
+		int status_code = 200;
+		
+		if ( ! file_found || filename == filenames[2])
+			status_code = 404;
 		if ( ! file_found )
 		{
-			headers = "HTTP/1.1 404 Not Found";
-			contents = "";
-		}
-		else
-		{
-			headers = "HTTP/1.1 200 OK \r\n";
-			headers.append ("Content-Type: text/html \r\n");
+			content_type = "";
 		}
 		
-		char* htmp = new char[100];
-		sprintf (htmp, "Content-Length: %ld \r\n\r\n", (int)contents.length());
-		headers.append(htmp);
-		delete [] htmp;
 		
-		client->send (headers.c_str(), (int)headers.length(), 0);
-		client->send (contents.c_str(), (int)contents.length(), 0);
+		http::response* response = new http::response (status_code, content_type);
+		response->set_body(contents);
+		
+		/**
+		 * write response to client
+		 */
+		std::string headersString = response->response_line();
+		std::map<std::string,std::string> headers = response->headers();
+		
+		for (std::map<std::string,std::string>::iterator it = headers.begin(); it != headers.end(); ++it)
+			headersString.append (it->first + ": " + it->second + "\r\n");
+		headersString.append("\r\n");
+		
+		client->send (headersString.c_str(), (int)headersString.length(), 0);
+		client->send (response->body().c_str(), (int)response->body().length(), 0);
 		
 		client->close();
 		delete client;
+		delete response;
 	}
 	
 	return 1;
