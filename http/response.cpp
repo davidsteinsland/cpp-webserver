2317edf731ed5e19d079e96f3f14dd85b9082541
davidsteinsland <david@davidsteinsland.net>
2013-04-12 00:01:13 +0200

    Fixed segfaults with int to string

--- http/response.cpp
+++ http/response.cpp
@@ -3,8 +3,94 @@
 
 #include <string>
 #include <map>
+#include <sstream>
 #include "response.h"
 
-std::map<std::string,std::string> http::response::status_codes(http::response::create_status_lines());
+std::map<int,std::string> http::response::status_codes;
+
+void http::response::create_status_lines()
+{
+	status_codes[200] = "OK";
+	status_codes[201] = "Created";
+	status_codes[202] = "Accepted";
+	status_codes[203] = "Non-Authoritative Information";
+	status_codes[204] = "No Content";
+	status_codes[205] = "Reset Content";
+	status_codes[206] = "Partial Content";
+	status_codes[301] = "Moved Permanently";
+	status_codes[302] = "Found";
+	status_codes[304] = "Not Modified";
+	status_codes[400] = "Bad Request";
+	status_codes[401] = "Unauthorized";
+	status_codes[403] = "Forbidden";
+	status_codes[404] = "Not Found";
+	status_codes[500] = "Internal Server Error";
+	status_codes[502] = "Bad Gateway";
+};
+
+http::response::response (int http_code, std::string type)
+{
+	this->create_status_lines();
+	
+	server_protocol = "HTTP/1.1";
+	status_code = http_code;
+	content_type = type;
+	
+	if (type.length() > 0)
+		this->set_header ("Content-Type", type);
+}
+
+http::response::~response()
+{
+	
+}
+
+void http::response::set_header (std::string k,std::string v)
+{
+	response_headers.insert (std::pair<std::string,std::string>(k,v));
+}
+
+void http::response::set_body(std::string body)
+{
+	// std::to_string() fails miserably with MinGW
+	std::stringstream ss;
+	ss << body.length();
+	this->set_header("Content-Length", ss.str());
+	content_body = body;
+};
+
+std::map<std::string,std::string> http::response::headers()
+{
+	return response_headers;
+}
+
+std::string http::response::response_line()
+{
+	std::stringstream ss;	
+	ss << server_protocol << ' ' << status_code << ' ' << this->status() << "\r\n";
+	
+	return ss.str();
+}
+
+std::string http::response::protocol()
+{
+	return server_protocol;
+}
+
+int http::response::code()
+{
+	return status_code;
+}
+
+std::string http::response::body()
+{
+	return content_body;
+}
+
+std::string http::response::status()
+{
+	return "";
+	return status_codes.find(status_code)->second;
+}
 
 #endif
--- http/response.h
+++ http/response.h
@@ -9,59 +9,29 @@ namespace http
 	class response
 	{
 		private:
-			static std::map<std::string,std::string> status_codes;
+			static std::map<int,std::string> status_codes;
+			
+			std::map<std::string,std::string> response_headers;
 			int status_code;
+			
+			std::string server_protocol;
 			std::string content_type;
 			std::string content_body;
 			
-			static std::map<std::string,std::string> create_status_lines()
-			{
-				std::map<std::string,std::string> m;
-				m["200"] = "OK";
-				m["201"] = "Created";
-				m["202"] = "Accepted";
-				m["203"] = "Non-Authoritative Information";
-				m["204"] = "No Content";
-				m["205"] = "Reset Content";
-				m["206"] = "Partial Content";
-				m["301"] = "Moved Permanently";
-				m["302"] = "Found";
-				m["304"] = "Not Modified";
-				m["400"] = "Bad Request";
-				m["401"] = "Unauthorized";
-				m["403"] = "Forbidden";
-				m["404"] = "Not Found";
-				m["500"] = "Internal Server Error";
-				m["502"] = "Bad Gateway";
-				
-				return m;
-			};
+			static void create_status_lines();
 		public:
-			response (int http_code, std::string type)
-			{
-				status_code = http_code;
-				content_type = type;
-			};
+			response (int,std::string);
+			~response();
 			
-			void set_body(std::string body)
-			{
-				content_body = body;
-			};
+			void set_header (std::string,std::string);
+			void set_body(std::string);
 			
-			int code()
-			{
-				return status_code;
-			}
-			
-			std::string content()
-			{
-				return content_body;
-			}
-			
-			std::string status()
-			{
-				return status_codes.find("" + status_code)->second;
-			}
+			std::map<std::string,std::string> headers();
+			std::string response_line();
+			std::string protocol();
+			int code();
+			std::string body();
+			std::string status();
 	};
 };
 
