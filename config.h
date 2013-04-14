9d7af4eb175c56a22d9e6a29bce29e83bdbb9af3
davidsteinsland <david@davidsteinsland.net>
2013-04-15 00:19:48 +0200

    Added config

---
+++ config.h
@@ -0,0 +1,14 @@
+#include <string>
+
+namespace config
+{
+	const std::string HTML_ROOT = "./public_html";
+	const std::string MODULES_ROOT = "./modules";
+	const int PORT = 80;
+	
+	#ifdef _WIN32
+		const std::string MODULE_EXT = ".dll";
+	#else
+		const std::string MODULE_EXT = ".so";
+	#endif
+};
