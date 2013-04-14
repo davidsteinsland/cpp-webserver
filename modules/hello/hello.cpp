d46b191e69f496ab63e2cb24198ae396a4efea00
davidsteinsland <david@davidsteinsland.net>
2013-04-15 00:32:26 +0200

    Added more dynamic pages

---
+++ modules/hello/hello.cpp
@@ -0,0 +1,34 @@
+/**
+ * Dynamic Page module for Windows
+ */
+ 
+#include "../../http/request.h"
+#include "../../http/response.h"
+#include <windows.h>
+#include <string>
+#include <sstream>
+
+int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void* lpReserved)
+{
+	return 1;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	return 0;
+}
+
+extern "C" __declspec (dllexport) int handle_request(http::request* request, http::response* response)
+{
+	response->set_status(200);
+	response->set_content_type ("text/html");
+	
+	std::string person = request->query_string("name", "Mr. No Name!");
+	
+	std::stringstream ss;
+	ss << "<h1>Hello, " << person << "!</h1>";
+	
+	response->set_body(ss.str());
+	
+	return 1;
+}
