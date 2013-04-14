8c20315cf93eb4b3bee6498f50efc1134158eb92
davidsteinsland <david@davidsteinsland.net>
2013-04-15 01:13:51 +0200

    Added String utility

---
+++ utils/stringutils.cpp
@@ -0,0 +1,33 @@
+#ifndef STRINGUTILS_CPP
+#define STRINGUTILS_CPP
+
+#include "stringutils.h"
+
+#include <cstdio>
+#include <string>
+#include <sstream>
+
+std::string utils::stringutils::urldecode (std::string &url)
+{
+	std::string ret;
+	char ch;
+	int i, j;
+	for (i = 0; i < url.length(); i++)
+	{
+		if (int (url[i]) == 37)
+		{
+			sscanf (url.substr (i + 1, 2).c_str(), "%x", &j);
+			ch = static_cast<char>(j);
+			ret += ch;
+			i = i + 2;
+		}
+		else if (url[i] == '+')
+			ret += ' ';
+		else
+			ret += url[i];
+	}
+	
+	return ret;
+}
+
+#endif
---
+++ utils/stringutils.h
@@ -0,0 +1,19 @@
+/**
+ * StringUtils
+ */
+#ifndef STRINGUTILS_H
+#define STRINGUTILS_H 
+
+#include <string>
+
+namespace utils
+{
+	class stringutils
+	{
+		public:
+			static std::string urldecode (std::string&);
+
+	};
+};
+
+#endif
